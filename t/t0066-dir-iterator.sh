bef2ccf3f7b4d392a0f6470ed7628a013716813c
Plato Kiorpelidis <kioplato@gmail.com>
2022-04-24 20:00:41 +0300

    t0066: shorter expected and actual output file names
    Change the names of expected-* and actual-* files to shorter ones to
    improve readability, avoiding line breaks. There is no reason to have
    descriptive expected-* and actual-* file names since subtests
    descriptions fulfill this need.
    
    If a test fails we can re-run it with --run flag and read its expected
    and actual outputs, since each subtest shares the same file names for
    expected and actual outputs.
    
    Signed-off-by: Plato Kiorpelidis <kioplato@gmail.com>

--- t/t0066-dir-iterator.sh
+++ t/t0066-dir-iterator.sh
@@ -16,7 +16,7 @@ test_expect_success 'setup -- dir w/ complex structure' '
 	>dir/d/e/d/a
 '
 test_expect_success 'dir-iterator should iterate through all files' '
-	cat >expected-iteration-sorted-output <<-EOF &&
+	cat >expected-sorted-out <<-EOF &&
 	[d] (a) [a] ./dir/a
 	[d] (a/b) [b] ./dir/a/b
 	[d] (a/b/c) [c] ./dir/a/b/c
@@ -30,10 +30,10 @@ test_expect_success 'dir-iterator should iterate through all files' '
 	[f] (d/e/d/a) [a] ./dir/d/e/d/a
 	EOF
 
-	test-tool dir-iterator ./dir >out &&
-	sort out >./actual-iteration-sorted-output &&
+	test-tool dir-iterator ./dir >actual-out &&
+	sort actual-out >actual-sorted-out &&
 
-	test_cmp expected-iteration-sorted-output actual-iteration-sorted-output
+	test_cmp expected-sorted-out actual-sorted-out
 '
 
 test_expect_success 'setup -- dir w/ three nested dirs w/ file' '
@@ -41,35 +41,34 @@ test_expect_success 'setup -- dir w/ three nested dirs w/ file' '
 	>dir2/a/b/c/d
 '
 test_expect_success 'dir-iterator should list files in the correct order' '
-	cat >expected-pre-order-output <<-EOF &&
+	cat >expected-out <<-EOF &&
 	[d] (a) [a] ./dir2/a
 	[d] (a/b) [b] ./dir2/a/b
 	[d] (a/b/c) [c] ./dir2/a/b/c
 	[f] (a/b/c/d) [d] ./dir2/a/b/c/d
 	EOF
 
-	test-tool dir-iterator ./dir2 >actual-pre-order-output &&
+	test-tool dir-iterator ./dir2 >actual-out &&
 
-	test_cmp expected-pre-order-output actual-pre-order-output
+	test_cmp expected-out actual-out
 '
 
 test_expect_success 'begin should fail upon inexistent paths' '
-	echo "dir_iterator_begin failure: ENOENT" >expected-inexistent-path-output &&
+	echo "dir_iterator_begin failure: ENOENT" >expected-out &&
 
-	test_must_fail test-tool dir-iterator ./inexistent-path \
-		>actual-inexistent-path-output &&
+	test_must_fail test-tool dir-iterator ./inexistent-path >actual-out &&
 
-	test_cmp expected-inexistent-path-output actual-inexistent-path-output
+	test_cmp expected-out actual-out
 '
 
 test_expect_success 'begin should fail upon non directory paths' '
 	>some-file &&
 
-	echo "dir_iterator_begin failure: ENOTDIR" >expected-non-dir-output &&
+	echo "dir_iterator_begin failure: ENOTDIR" >expected-out &&
 
-	test_must_fail test-tool dir-iterator ./some-file >actual-non-dir-output &&
+	test_must_fail test-tool dir-iterator ./some-file >actual-out &&
 
-	test_cmp expected-non-dir-output actual-non-dir-output
+	test_cmp expected-out actual-out
 '
 
 test_expect_success POSIXPERM,SANITY 'setup -- dir w/ dir w/o perms w/ file' '
@@ -77,28 +76,28 @@ test_expect_success POSIXPERM,SANITY 'setup -- dir w/ dir w/o perms w/ file' '
 	>dir3/a/b
 '
 test_expect_success POSIXPERM,SANITY 'advance should not fail on errors by default' '
-	cat >expected-no-permissions-output <<-EOF &&
+	cat >expected-out <<-EOF &&
 	[d] (a) [a] ./dir3/a
 	EOF
 
 	chmod 0 dir3/a &&
 
-	test-tool dir-iterator ./dir3 >actual-no-permissions-output &&
-	test_cmp expected-no-permissions-output actual-no-permissions-output &&
+	test-tool dir-iterator ./dir3 >actual-out &&
+	test_cmp expected-out actual-out &&
+
 	chmod 755 dir3/a
 '
 test_expect_success POSIXPERM,SANITY 'advance should fail on errors, w/ pedantic flag' '
-	cat >expected-no-permissions-pedantic-output <<-EOF &&
+	cat >expected-out <<-EOF &&
 	[d] (a) [a] ./dir3/a
 	dir_iterator_advance failure
 	EOF
 
 	chmod 0 dir3/a &&
 
-	test_must_fail test-tool dir-iterator --pedantic ./dir3 \
-		>actual-no-permissions-pedantic-output &&
-	test_cmp expected-no-permissions-pedantic-output \
-		actual-no-permissions-pedantic-output &&
+	test_must_fail test-tool dir-iterator --pedantic ./dir3 >actual-out &&
+	test_cmp expected-out actual-out &&
+
 	chmod 755 dir3/a
 '
 
@@ -110,7 +109,7 @@ test_expect_success SYMLINKS 'setup -- dir w/ symlinks w/o cycle' '
 	ln -s ../b dir4/a/f
 '
 test_expect_success SYMLINKS 'dir-iterator should not follow symlinks by default' '
-	cat >expected-no-follow-sorted-output <<-EOF &&
+	cat >expected-sorted-out <<-EOF &&
 	[d] (a) [a] ./dir4/a
 	[d] (b) [b] ./dir4/b
 	[d] (b/c) [c] ./dir4/b/c
@@ -119,13 +118,13 @@ test_expect_success SYMLINKS 'dir-iterator should not follow symlinks by default
 	[s] (a/f) [f] ./dir4/a/f
 	EOF
 
-	test-tool dir-iterator ./dir4 >out &&
-	sort out >actual-no-follow-sorted-output &&
+	test-tool dir-iterator ./dir4 >actual-out &&
+	sort actual-out >actual-sorted-out &&
 
-	test_cmp expected-no-follow-sorted-output actual-no-follow-sorted-output
+	test_cmp expected-sorted-out actual-sorted-out
 '
 test_expect_success SYMLINKS 'dir-iterator should follow symlinks w/ follow flag' '
-	cat >expected-follow-sorted-output <<-EOF &&
+	cat >expected-sorted-out <<-EOF &&
 	[d] (a) [a] ./dir4/a
 	[d] (a/f) [f] ./dir4/a/f
 	[d] (a/f/c) [c] ./dir4/a/f/c
@@ -135,10 +134,10 @@ test_expect_success SYMLINKS 'dir-iterator should follow symlinks w/ follow flag
 	[f] (a/e) [e] ./dir4/a/e
 	EOF
 
-	test-tool dir-iterator --follow-symlinks ./dir4 >out &&
-	sort out >actual-follow-sorted-output &&
+	test-tool dir-iterator --follow-symlinks ./dir4 >actual-out &&
+	sort actual-out >actual-sorted-out &&
 
-	test_cmp expected-follow-sorted-output actual-follow-sorted-output
+	test_cmp expected-sorted-out actual-sorted-out
 '
 
 test_expect_success SYMLINKS 'setup -- dir w/ symlinks w/ cycle' '
